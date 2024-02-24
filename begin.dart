601cf2d39928f5cf43a851b650af0a186e231bcf
geniusahyan <aslamalam078523@gmail.com>
2024-02-24 11:29:27 +0530

    variable in dart

---
+++ begin.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Aslam Ahyan";
+  int age = 19;
+  dynamic some = "anything";
+  print(age.isEven);
+  print(age.runtimeType);
+  print(
+      "Your name is: $name and your age is: $age and this is dynamic value: $some ");
+}
